50c2b70abc53d7152406b2f941c29d6d11b3978d
yehoshua <y0587828030@gmail.com>
2026-07-24 11:16:39 +0300

    2-logical - part 2

---
+++ 2-logical opertators.py
@@ -0,0 +1,56 @@
+# # part 1
+# #1
+# is_online = True 
+# has_access = False
+
+# print(is_online and has_access)
+
+# #2
+# print(is_online or has_access)
+
+# #3
+# status = False
+# print(not(status))
+
+#4
+# age = 20 
+# has_id = True
+# if age >= 18 and has_id:
+#     print("welcome")
+
+# #5
+# level = 3
+# print(1 <= level <= 5)
+
+# #6
+# a = 0
+# b = "hello"
+# c = ""
+
+
+# print(bool(a))
+# print(bool(b))
+# print(bool(c))
+
+# #7
+# x = None
+# y = 42
+
+# print(x or y)
+
+# #8
+# username = ""
+# default = "guest"
+
+# username_final = username or default
+# print(username_final)
+
+# #9
+# print(True and False or True)
+
+#10
+score = 75
+
+print(score >= 60 and score <=100)
+
+
